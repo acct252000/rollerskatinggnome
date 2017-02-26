c6224b8a4e50e71583eac1e0b9a362004db07d6a
Christine Stoner <acct252000@gmail.com>
2017-02-26 14:14:25 -0600

    Add documentation and beautify index.php

--- index.php
+++ index.php
@@ -13,17 +13,17 @@
 
 <body>
 <?php
-
+//connect to database
 $dbopts = parse_url(getenv('DATABASE_URL'));
 $connection_string = 'dbname='.ltrim($dbopts["path"],'/').' host='.$dbopts["host"].' port='.$dbopts["port"].' user='.$dbopts["user"].' password='.$dbopts["pass"];
 
 $dbconn = pg_connect($connection_string);
-
+//select current skates
 $result = pg_exec($dbconn, "select * from currentskates");
 $numrows = pg_numrows($result);
 
 $skates = array();
-
+//push result into skate array
 for($ri = 0; $ri < $numrows; $ri++) {
     
     $row = pg_fetch_array($result, $ri);
@@ -39,83 +39,68 @@ pg_close($dbconn);
   var skate_data = <?php echo json_encode($skates); ?>; 
   var skate_detail = false;
 </script>
-
-        <header>
-           
-            <div> <img id="gnome" src="img/gnome.png" alt="rollerskating gnome">Rollerskating Gnome</div>
-                <span id="weather_attribute" data-bind="text: weatherAttribString"></span></header>
-         
-            <div id="bar">
-
-	    	
-            <form id="header_form"> <span id="hamburger" class="glyphicon glyphicon-menu-hamburger header_icon" aria-hidden="true" data-bind="click: showMenu"></span>
-<label for="name-filter">Search on Map:</label><span class="glyphicon glyphicon-list-alt header_icon" aria-hidden="true"></span>
-            <input type="text" placeholder="Local trail" id="name-filter" data-bind="value: filter, valueUpdate: 'afterkeydown'" />
- 
-            <label for="zip-code" id="map_label">Re-center:</label><span class="glyphicon glyphicon-globe header_icon" aria-hidden="true"></span>
-            <input type="text"  placeholder="City, State or Zip" name="zipCode" id="zip-code" data-bind="textInput: zipCode" /><button id="map_button" class="btn btn-sm" data-bind="click: resetMap">Go!</button>
-	    <span id="all_filter"><label for="all-filter">Search All Skates:</label><span class="glyphicon glyphicon-search header_icon" aria-hidden="true"></span>
-	<input type="text"  placeholder="Any trail" data-bind="value: allFilter, valueUpdate: 'afterkeydown'" /></span>
-        
-</div>
-
-
-
-     <div class="wrapper">
-
-<nav class="corners" data-bind="css: { open: showNavBar()}">
-       
-        <ul data-bind="foreach: filteredSkates" class="allskatelinks corners" id="myskatelinks">
-            <li class="skatelink" data-bind="text: trailName, click: $parent.clickSkate"></li>
-        </ul>
-       <hr>
-       <div class="navform">	
-       <form">
-       Miles:<br>
-  
-                       <input class="miles_input" type="text" name="minimum miles" id="min-miles" data-bind="textInput: minMiles" />
-	    <label for "min-miles"></label> to 
-	    
-            <input class="miles_input" type="text" name="maximum miles" id="max-miles" data-bind="textInput: maxMiles" />
-            <label for="max-miles"></label>
-           
-
-	<div class="btn-group">
-        <button class="btn btn-default btn-xs left-btn" data-bind="click: filterResults"><span class="glyphicon glyphicon-filter"></span></button>
-        <button class="btn btn-default right-btn btn-xs" data-bind="click: unFilterResults"><span class="glyphicon glyphicon-remove" aria-hidden="true"></button>
-	</div>
-	   
-	</form>
-	</div>
-     <hr>
-     <div class="new_skate_link">
-    <a href="https://blooming-badlands-10202.herokuapp.com/newtrail.php">Suggest a New Skate!</a>
+    <!--   Header section including search bars -->
+    <header>
+        <div> <img id="gnome" src="img/gnome.png" alt="rollerskating gnome">Rollerskating Gnome
+        </div>
+        <span id="weather_attribute" data-bind="text: weatherAttribString">
+        </span>
+    </header>       
+        <div id="bar">
+            <form id="header_form"> 
+                <span id="hamburger" class="glyphicon glyphicon-menu-hamburger header_icon" aria-hidden="true" data-bind="click: showMenu">    
+                </span>
+                <label for="name-filter">Search on Map:</label><span class="glyphicon glyphicon-list-alt header_icon" aria-hidden="true"></span>
+                <input type="text" placeholder="Local trail" id="name-filter" data-bind="value: filter, valueUpdate: 'afterkeydown'" />
+                <label for="zip-code" id="map_label">Re-center:</label><span class="glyphicon glyphicon-globe header_icon" aria-hidden="true"></span>
+                <input type="text"  placeholder="City, State or Zip" name="zipCode" id="zip-code" data-bind="textInput: zipCode" /><button id="map_button" class="btn btn-sm" data-bind="click: resetMap">Go!</button>
+	            <span id="all_filter"><label for="all-filter">Search All Skates:</label><span class="glyphicon glyphicon-search header_icon" aria-hidden="true"></span>
+	            <input type="text"  placeholder="Any trail" data-bind="value: allFilter, valueUpdate: 'afterkeydown'" /></span>
+            </form>
+        </div>
+    <div class="wrapper">
+        <!--   Left navbar which flys out on lower screen sizes-->
+        <nav class="corners" data-bind="css: { open: showNavBar()}">
+            <ul data-bind="foreach: filteredSkates" class="allskatelinks corners" id="myskatelinks">
+                <li class="skatelink" data-bind="text: trailName, click: $parent.clickSkate"></li>
+            </ul>
+            <hr>
+            <div class="navform">	
+                <form>
+                    Miles:<br>
+                    <input class="miles_input" type="text" name="minimum miles" id="min-miles" data-bind="textInput: minMiles" />
+	               <label for "min-miles"></label> to 
+	               <input class="miles_input" type="text" name="maximum miles" id="max-miles" data-bind="textInput: maxMiles" />
+                   <label for="max-miles"></label>
+                   <div class="btn-group">
+                        <button class="btn btn-default btn-xs left-btn" data-bind="click: filterResults"><span class="glyphicon glyphicon-filter"></span></button>
+                        <button class="btn btn-default right-btn btn-xs" data-bind="click: unFilterResults"><span class="glyphicon glyphicon-remove" aria-hidden="true"></button>
+	               </div>
+	           </form>
+	       </div>
+           <hr>
+           <div class="new_skate_link">
+                <a href="https://blooming-badlands-10202.herokuapp.com/newtrail.php">Suggest a New Skate!</a>
+            </div>   
+            <div id="menu_arrow">
+                <span class="glyphicon glyphicon-circle-arrow-left" aria-hidden="true" data-bind="click: hideMenu"></span>
+            </div>
+       </nav>
+       <!--   List of all skates is empty unless value returned from Search All Skates in menu bar-->
+        <div class="all_skates_nav corners" data-bind="if: allFilteredSkates">
+            <ul data-bind="foreach: allFilteredSkates" class="allskatelinks" id="myallskatelinks">
+                <li class="skatelink" data-bind="text: trailName, click: $parent.clickSkate"></li>
+            </ul>
+        </div>
+        <!--   Section for google map -->
+        <div id="main">
+             <div id="map">
+            </div>
+        </div>
     </div>
-        <div id="menu_arrow"><span class="glyphicon glyphicon-circle-arrow-left" aria-hidden="true" data-bind="click: hideMenu"></span></div>
-    </nav>
-
-<div class="all_skates_nav corners" data-bind="if: allFilteredSkates">
-
-<ul data-bind="foreach: allFilteredSkates" class="allskatelinks" id="myallskatelinks">
-            <li class="skatelink" data-bind="text: trailName, click: $parent.clickSkate"></li>
-        </ul>
-</div>
-   
-   <div id="main">
-        
-
-        <div id="map" ></div>
-    </div>
-   
-</div>
-    <script>
-    </script>
-
-   
 
     <script src="js/knockout.js"></script>
     <script src="js/main.js"></script>
-
     <script src="js/jquery.min.js"></script>
      <script defer async src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCkzEV_2vZYplUr-ceO0gt7F0q5y_k5eX8&v=3&callback=initMap">
     </script>
